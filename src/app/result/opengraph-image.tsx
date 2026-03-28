afa36dc422839d8b3406d4b29a7d342480e410d2
addicted-yt <65751062@qq.com~>
2026-03-28 20:24:54 +0800

    修复部署问题

--- src/app/result/opengraph-image.tsx
+++ src/app/result/opengraph-image.tsx
@@ -3,6 +3,7 @@ import { ImageResponse } from "next/og";
 export const alt = "Happy Birthday";
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
+export const dynamic = "force-dynamic";
 
 export default async function Image({
   searchParams,
